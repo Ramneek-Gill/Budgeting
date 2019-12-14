a0f0f0e0ff3064a5b2c03d05d2d53c65bb3a6c31
Ramneekg <42669256+Ramneekg@users.noreply.github.com>
2019-12-13 21:22:57 -0800

    changed fakeTransactions

--- src/services/fakeTransactionService.js
+++ src/services/fakeTransactionService.js
@@ -4,129 +4,129 @@ const transactions = [
   {
     _id: "5b21ca3eeb7f6fbccd471816",
     item: "Groceries",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+    category: { _id: "5df4707cb20fe10017f619c8", name: "Food" },
     cost: 70
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
     item: "Video Game",
-    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
+    category: { _id: "5df47089b20fe10017f619c9", name: "Entertainment" },
     cost: 80
   },
   {
     _id: "5b21ca3eeb7f6fbccd471818",
     item: "Starbucks",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+    category: { _id: "5df47092b20fe10017f619ca", name: "Utilities" },
     cost: 6
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718179",
-    item: "Poker",
-    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
-    cost: 15
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718180",
-    item: "Boston Pizza",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 32
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718181",
-    item: "Hockey Game",
-    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
-    cost: 50
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718182",
-    item: "Sports Bar",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 18
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718183",
-    item: "Online Game Subscription",
-    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
-    cost: 15
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718184",
-    item: "Mcdonalds",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 8
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718185",
-    item: "Pizza Hut",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 32
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718186",
-    item: "Starbucks",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 8
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718187",
-    item: "KFC",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 14
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718188",
-    item: "Groceries",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 15
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718189",
-    item: "Taco Bell",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 12
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718190",
-    item: "Fruit",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 20
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718191",
-    item: "Veggies",
-    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
-    cost: 14
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718192",
-    item: "Electricity",
-    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
-    cost: 150
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718193",
-    item: "Water",
-    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
-    cost: 230
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718194",
-    item: "Internet",
-    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
-    cost: 50
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718195",
-    item: "CableTV",
-    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
-    cost: 50
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd4718196",
-    item: "Water",
-    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
-    cost: 210
   }
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718179",
+  //   item: "Poker",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
+  //   cost: 15
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718180",
+  //   item: "Boston Pizza",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 32
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718181",
+  //   item: "Hockey Game",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
+  //   cost: 50
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718182",
+  //   item: "Sports Bar",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 18
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718183",
+  //   item: "Online Game Subscription",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Entertainment" },
+  //   cost: 15
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718184",
+  //   item: "Mcdonalds",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 8
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718185",
+  //   item: "Pizza Hut",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 32
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718186",
+  //   item: "Starbucks",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 8
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718187",
+  //   item: "KFC",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 14
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718188",
+  //   item: "Groceries",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 15
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718189",
+  //   item: "Taco Bell",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 12
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718190",
+  //   item: "Fruit",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 20
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718191",
+  //   item: "Veggies",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Food" },
+  //   cost: 14
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718192",
+  //   item: "Electricity",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
+  //   cost: 150
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718193",
+  //   item: "Water",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
+  //   cost: 230
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718194",
+  //   item: "Internet",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
+  //   cost: 50
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718195",
+  //   item: "CableTV",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
+  //   cost: 50
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd4718196",
+  //   item: "Water",
+  //   category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Utilities" },
+  //   cost: 210
+  // }
 ];
 
 export function getTransactions() {
