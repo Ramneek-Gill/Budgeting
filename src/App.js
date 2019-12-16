80bbf03abe4d898b14a53a1f694b0c0a93c16714
Ramneekg <42669256+Ramneekg@users.noreply.github.com>
2019-12-16 11:04:42 -0800

    redirecttrans

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
             {/* <Route path="/subscriptions" component={Subscriptions} /> */}
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/transactions" />
+            <Redirect from="/profile" exact to="/transactions" />
             <Redirect to="/not-found" />
           </Switch>
         </main>
